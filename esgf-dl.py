98033245e06cea86f9c4fd52b83c89c71dffc7f9
onnyyonn <anamitra.saha21@gmail.com>
2021-05-31 04:26:12 +0530

    Checksum validation in fallback download

--- esgf-dl.py
+++ esgf-dl.py
@@ -9,24 +9,52 @@ save_path = "path/to/download/" # Make sure '/' is there at the end of the path
 # Settings end here =========================================================
 
 # Import dependencies
-import json, os, requests, tqdm
+import json, os, requests, hashlib
+from tqdm import tqdm
 
 # Load file list
 with open(file_list, 'r') as f:
     files = json.load(f)
 
-# Define a function for download
+# Define a download method
 def download(url, filename, save_path, checksum, checksum_type):
     r = os.system("aria2c " + url +
                   " --out=" + filename +
                   " --dir=" + save_path +
-                  " --check-integrity --checksum=" + checksum_type + "=" + checksum +
+                  " --check-integrity --checksum=" + checksum_type.lower() + "=" + checksum +
                   " --file-allocation=none" +
                   " --max-connection-per-server=8" +
                   " --max-concurrent-downloads=15" +
                   " --optimize-concurrent-downloads")
     return r
 
+# Define a fallback download method
+def download_fallback(url, filename, save_path, filesize, checksum, checksum_type):
+    print("Attempting download with fallback method...", flush=True)
+    r = requests.get(url, stream=True)
+    block_size = 1024*1024 #1 MiB
+    with open(save_path+filename, 'wb') as f:
+        for block in tqdm(r.iter_content(block_size), total=filesize//block_size, unit='MiB', ascii=True):
+            f.write(block)
+
+    validated = False
+    if checksum_type.lower() not in hashlib.algorithms_guaranteed:
+        print("Hashing algorithm isn't supported. Skipping integrity check...", flush=True)
+    else:
+        algorithm = getattr(hashlib, checksum_type.lower())()
+        print("Validating checksum...", flush = True)
+        with open(save_path+filename, 'rb') as f:
+            for block in tqdm(iter(lambda: f.read(block_size), b""), total=filesize//block_size, unit='MiB', ascii=True):
+                algorithm.update(block)
+
+        if algorithm.hexdigest() == checksum:
+            print("File is successfully validated against the hash!", flush=True)
+            validated = True
+        else:
+            print("Error validating file against the hash!", flush=True)
+
+    return r, validated
+    
 # download
 for file in files:
     if os.path.isfile(save_path+file["filename"]):
@@ -43,10 +71,5 @@ for file in files:
         r1 = download(" ".join(file["url"]), file["filename"], save_path, file["checksum"], file["checksum_type"].lower().replace("sha", "sha-"))
         
         if r1:
-        # Fallback method for download
-            print("Attempting download with fallback method...", flush=True)
-            r2 = requests.get(file["url"][0], stream=True)
-            block_size = 1024*1024
-            with open(save_path+file["filename"], 'wb') as f:
-                for data in tqdm.tqdm(r2.iter_content(block_size), total=file["size"]//block_size, unit='MiB', ascii=True):
-                    f.write(data)
+            # Fallback method for download
+            r2, v = download_fallback(file["url"][0],file["filename"], save_path, file["size"], file["checksum"], file["checksum_type"].lower())
